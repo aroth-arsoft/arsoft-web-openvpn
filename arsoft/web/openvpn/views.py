38a8d1b46f5e7a824f6fa01aeba3da4e3711bdc8
Andreas Roth <aroth@arsoft-online.com>
2014-04-18 10:26:37 +0200

    create wrapper objects for usage in template with the required details prepared for display as html

--- arsoft/web/openvpn/views.py
+++ arsoft/web/openvpn/views.py
@@ -9,37 +9,69 @@ import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
+class ConfigItem(object):
+    def __init__(self, hub, config_file):
+        self.hub = hub
+        self.config_file = config_file
+        self.name = self.config_file.name
+        self.filename = self.config_file.filename
+        self.is_running = self.hub.config.is_running(self.name)
+        self.autostart = True if self.name in self.hub.systemconfig.autostart else False
+        self.status_file_obj = arsoft.openvpn.StatusFile(config_file=self.config_file)
+        self.status_version = self.config_file.status_version
+        self.status_file = self.config_file.status_file
+        self.logfile = self.config_file.logfile if self.config_file.logfile else self.config_file.logfile_append
+        self.server = self.config_file.server
+        self.routes = []
+        for (network, netmask) in self.config_file.routes:
+            self.routes.append( '%s/%s' % (network, netmask))
+        self.routing_table = []
+        if self.status_file_obj.routing_table:
+            for entry in self.status_file_obj.routing_table:
+                self.routing_table.append(str(entry))
+
+        self.client_config_directory = self.config_file.client_config_directory if self.server else None
+        self.connection_state = self.status_file_obj.state.long_state
+        self.connected_clients = []
+        self.configured_clients = []
+        if self.server:
+            if self.status_file_obj.connected_clients:
+                for (clientname, clientinfo) in self.status_file_obj.connected_clients.iteritems():
+                    self.connected_clients.append(str(clientinfo))
+            if self.config_file.client_config_files:
+                for (clientname, ccdfile) in self.config_file.client_config_files.iteritems():
+                    self.configured_clients.append(clientname)
+
+
+
+class ConfigHub(object):
+    def __init__(self):
+        self.config = arsoft.openvpn.Config()
+        self.systemconfig = arsoft.openvpn.SystemConfig()
+
+    def list(self, selected_vpns=[]):
+        ret = []
+        if not selected_vpns:
+            selected_vpns = self.config.names
+
+        logger.error('self.config.names=%s' % self.config.names)
+        logger.error('selected_vpns=%s' % selected_vpns)
+        for vpnname in selected_vpns:
+            item = ConfigItem(self, arsoft.openvpn.ConfigFile(config_name=vpnname))
+            ret.append(item)
+        return ret
+
 def home(request):
 
-    selected_vpns = ['myhome']
-    _config = arsoft.openvpn.Config()
-    _systemconfig = arsoft.openvpn.SystemConfig()
-    all_vpn_names = _config.names
-    if len(selected_vpns) == 0:
-        selected_vpns = all_vpn_names
-
-    class ConfigItem(object):
-        def __init__(self, name, config_file, is_running, status_file, autostart=False):
-            self.name = name
-            self.config_file = config_file
-            self.is_running = is_running
-            self.status_file = status_file
-            self.autostart = autostart
-
-    config_list = []
-
-    for vpnname in selected_vpns:
-        config_file = arsoft.openvpn.ConfigFile(config_name=vpnname)
-        is_running = _config.is_running(vpnname)
-        autostart = True if vpnname in _systemconfig.autostart else False
-        status_file = arsoft.openvpn.StatusFile(config_file=config_file)
-        config_list.append( ConfigItem(vpnname, config_file=config_file, is_running=is_running, status_file=status_file, autostart=autostart) )
-
+    hub = ConfigHub()
     title = 'OpenVPN status'
+
+    logger.error('config_list=%s' % hub.list())
 
     t = loader.get_template('home.html')
     c = RequestContext( request, { 
-        'config_list':config_list,
+        'config_list':hub.list(),
         'title':title
         })
     return HttpResponse(t.render(c))
