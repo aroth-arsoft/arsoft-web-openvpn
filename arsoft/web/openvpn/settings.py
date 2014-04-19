bbe6bab40b2e63dc81f62ce42f9eb4fb0fe1d6ef
Andreas Roth <aroth@arsoft-online.com>
2014-04-19 09:28:57 +0200

    some more renaming

--- arsoft/web/openvpn/settings.py
+++ arsoft/web/openvpn/settings.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
 
-# Django settings for arsoft.web.kpasswd project.
+# Django settings for arsoft.web.openvpn project.
 from arsoft.web.utils import initialize_settings
 
 # use initialize_settings from arsoft.web.utils to get the initial settings
--- arsoft/web/openvpn/wsgi.py
+++ arsoft/web/openvpn/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for arsoft.web.kpasswd project.
+WSGI config for arsoft.web.openvpn project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arsoft.web.kpasswd.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arsoft.web.openvpn.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
