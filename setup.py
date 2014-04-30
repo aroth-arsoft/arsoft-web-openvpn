314d4b59492e043d39af7c127c331d056950420e
Andreas Roth <aroth@arsoft-online.com>
2014-04-30 07:05:26 +0200

    prepare next version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-web-openvpn',
-		version='0.8',
+		version='0.9',
 		description='shows the status of OpenVPN via web',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
