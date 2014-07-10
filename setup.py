101ee7f8cf0e4af0453cf4b45fd9fa3cd4d14453
Andreas Roth <aroth@arsoft-online.com>
2014-07-10 14:22:24 +0200

    build with debug url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-web-openvpn',
-		version='0.9',
+		version='0.10',
 		description='shows the status of OpenVPN via web',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
