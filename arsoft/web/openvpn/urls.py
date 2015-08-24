e46ac2abe2afad6cdce3107d7e22bd9de5b70343
Andreas Roth <andreas.roth@fastprotect.net>
2015-08-24 14:00:10 +0200

    fixed debug url handler

--- arsoft/web/openvpn/urls.py
+++ arsoft/web/openvpn/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from arsoft.web.utils import django_debug_urls
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,7 +18,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
 
-from arsoft.web.utils import django_debug_urls
-django_debug_urls(__name__, __file__)
+    url(r'^debug/', include(django_debug_urls())),
+)
